39bbb2c42665ac66e4484dc6d68a1a7a1caee61c
simonboyko <simonderus@gmail.com>
2018-05-20 04:08:43 +0300

    Добавлено решение задачи 2 для занятия 1.1

---
+++ lesson_1.1/task_2/read-all.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const conf = {encoding: 'utf8'};
+
+module.exports = readAll = (path) => {
+  let allFiles = [];
+  return new Promise((done, fail) => {
+    fs.readdir(path, conf, (err, files) => {
+      if (err) {
+        fail(err);
+      } else {
+        files.forEach((file, index) => {
+          allFiles.push({name: file, content: fs.readFileSync(path + file, conf, (err) => {
+            if (err) fail(err);
+          })});
+        });
+        done(allFiles);
+      }
+    });
+  });
+};
